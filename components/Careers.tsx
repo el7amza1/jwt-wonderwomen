5f8a5a2de984e0d6a97a58b6d2892ac9ffbaf0d0
Marwan-98 <marwanashrafzaki98@gmail.com>
2022-09-26 10:38:30 +0200

    env

--- components/Careers.tsx
+++ components/Careers.tsx
@@ -5,45 +5,6 @@ import CareersH from "./CareersHeader";
 import { createClient } from "next-sanity";
 import { Job } from "../types";
 
-const positions = [
-  {
-    id: 1,
-    title: "Back End Developer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Engineering",
-    closeDate: "2020-01-07",
-    closeDateFull: "January 7, 2020",
-  },
-  {
-    id: 2,
-    title: "Front End Developer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Engineering",
-    closeDate: "2020-01-07",
-    closeDateFull: "January 7, 2020",
-  },
-  {
-    id: 3,
-    title: "User Interface Designer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Design",
-    closeDate: "2020-01-14",
-    closeDateFull: "January 14, 2020",
-  },
-  {
-    id: 4,
-    title: "User Interface Designer",
-    type: "Full-time",
-    location: "Remote",
-    department: "Design",
-    closeDate: "2020-01-14",
-    closeDateFull: "January 14, 2020",
-  },
-];
-
 export default function Careers() {
   const [jobs, setJobs] = useState<Job[]>([]);
   const client = createClient({
